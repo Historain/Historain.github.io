43baaa88206fc61156533806c85810309d94f6b0
Historain <39673454+Historain@users.noreply.github.com>
2018-06-22 16:59:02 +0500

    Create base.js

---
+++ base.js
@@ -0,0 +1,17 @@
+var marker = L.marker([54.7239839,55.9475713]).addTo(mymap) 
+.bindPopup("<strong>БГПУ им. М. Акмуллы</strong><br />Адрес: ул. Октябрьской Революции, 3А").openPopup();
+
+var marker = L.marker([54.7198165,55.9325813]).addTo(mymap) 
+.bindPopup("<strong>БГУ</strong><br />Адрес: ул. Заки Валиди, 32").openPopup();
+
+var marker = L.marker([54.7216345,55.9400268]).addTo(mymap) 
+.bindPopup("<strong>БГМУ</strong><br />Адрес: ул. ул. Ленина, 3").openPopup();
+
+var marker = L.marker([54.72498,55.942506]).addTo(mymap) 
+.bindPopup("<strong>УГАТУ Корпус №1</strong><br />Адрес: ул. Карла Маркса, 12").openPopup();
+
+var marker = L.marker([54.7361738,55.9820204]).addTo(mymap) 
+.bindPopup("<strong>БГАУ</strong><br />Адрес: ул. ул. 50 лет Октября, 34").openPopup();
+
+var marker = L.marker([54.8172218,56.0592491]).addTo(mymap) 
+.bindPopup("<strong>УГНТУ</strong><br />Адрес: ул. ул. Космонавтов, 5/1").openPopup();
